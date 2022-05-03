fe4a50e1983352ecea22b859f7c94b864fd858e5
Abhishek <idonttellanyonemyemail@gmail.com>
2022-05-03 14:39:14 +0530

    FIX: server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+console.log("dev", dev);
 
 app
   .prepare()
