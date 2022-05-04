c8421fd75fff76c07e778011511bd2fe51a5de80
Abhishek <idonttellanyonemyemail@gmail.com>
2022-05-04 21:48:19 +0530

    REMOVE: proxy

--- context/index.js
+++ context/index.js
@@ -68,7 +68,9 @@ const Provider = ({ children }) => {
 
   useEffect(() => {
     const getCsrfToken = async () => {
-      const { data } = await axios.get("/api/csrf-token");
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/csrf-token`
+      );
       // console.log("CSRF", data);
       axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
     };
